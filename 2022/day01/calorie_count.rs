a45b30e0101563e2b9b957ee2d5299ca50b17172
Fennel Kora <fencore@zoner.work>
2022-12-02 10:52:59 -0500

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   2022/day01/calorie_count.rs
    	new file:   2022/day01/day01_algo.docx
    	new file:   2022/day01/input.txt

---
+++ 2022/day01/calorie_count.rs
@@ -0,0 +1,11 @@
+use std::io::prelude::*;
+use std::fs;
+use std::path::*;
+
+fn main() {
+    let mut _max_cals = 0;
+    let path = Path::new("input.txt");
+    let display = path.display();
+    
+    let mut file
+}

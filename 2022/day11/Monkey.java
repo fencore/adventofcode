a0d6414f88456efae8ccc0d70b4b3fc7b0582ca9
Fennel Kora <fencore@zoner.work>
2022-12-12 17:36:24 -0500

    monke

---
+++ 2022/day11/Monkey.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Monkey {
+    private List<Integer> items;
+    private char inspectOp;
+    private int inspectFactor;
+    private int trueDest;
+    private int falseDest;
+
+    public Monkey (List<Integer> arr, char op, int factor, int trueD, int falseD) {
+        items = arr;
+        inspectOp = op;
+        inspectFactor = factor;
+        trueDest = trueD;
+        falseDest = falseD;
+    }
+
+    private parseOperator()
+}
